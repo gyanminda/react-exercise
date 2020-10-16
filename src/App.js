73d9bbe2f4ad2c3b85c195faca4fa079d7543ebc
Maggie Yogeshvar <maggieyogeshvar@Maggies-MacBook-Pro.local>
2020-10-16 11:47:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // TODO: Add other states for the application.
+    const [username, setUsername] = React.useState('');
+
+    // TODO: Console.log(event) and get the object which has value of the input field.
+    const handleUsername = event => setUsername();
+
+    const handleApiCall = (username, password) => {
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            // TODO: add username and password.
+            body: JSON.stringify({})
+        };
+        fetch("api call", requestOptions)
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    // success response will be in result.
+                },
+                (error) => {
+                    // failure response will be in error.
+                }
+            )
+    }
+
+    return (
+        <div className="App">
+            <div className="task_input">
+                <h1>Login Form</h1>
+                {/* TODO: Add a conditional rendering based on the api call. Handle the state and then do a conditional rendering. 
+                    Ref: https://reactjs.org/docs/conditional-rendering.html
+                */}
+                <input type="text" id="username" name="username" onChange={handleUsername} value={username} placeholder="Username" />
+                <br />
+                <input type="password" id="password" name="password" placeholder="Password" />
+                <div className="center">
+                    <button type="button" className="btn btn-success" id="add-task-btn" onClick={handleApiCall}>Login</button>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
